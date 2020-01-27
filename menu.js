a6d30276ce3f3cb99159b499c18d3bc211a6ba3b
christophe <josse_christophe@yahoo.fr>
2020-01-27 11:26:58 +0100

    premier module js menu

---
+++ menu.js
@@ -0,0 +1,12 @@
+const menu = `
+<ul>
+    <li>
+        <a href="index.html">accueil</a>
+    </li>
+    <li>
+        <a href="index.html">contact</a>
+    </li>
+</ul>
+`;
+
+document.getElementById("navigation").innerHTML = menu ;
